4118ea7b863f724d95294ff01e960606c6f31e01
abi4679553 <abivengadajalam7708@gmail.com>
2026-01-06 20:57:11 +0530

    hello

--- Day1.js
+++ Day1.js
@@ -1,9 +1,2 @@
-// console.log("abi");
-// alert("hell java script");
-// <button onClick="hello()">click me</button>
-
-// function hello() {
-//     alert("submitted")
-// }
-    let name = window.prompt("ok");
+console.log("abi")
 
